e10c82ee95f093218001f838404d30994a62e31b
Jaime A <jaimeangel1097@gmail.com>
2024-01-19 17:12:34 -0500

    Type Array

---
+++ notes/Datos primitivos/05-arrays.ts
@@ -0,0 +1,35 @@
+(() => {
+  const prices = [1,2,3,4,5,6, 'hola', true];
+
+  //Not possible do it
+  // prices.push('jkgdsakjgd');
+  // prices.push(true);
+  // prices.push({});
+
+  prices.push(12313131);
+  let products = ['hola', true];
+  products.push(false);
+
+  //When writing booleans, numbers, and strings, it must be done explicitly.
+  const mixed: (number | string | boolean)[] = ['hola', true];
+  //now, you can push number,string or boolean
+  mixed.push(12);
+  mixed.push(true)
+  mixed.push('accepted')
+
+  let numbers = [1,2,3,4,5,6,7,8];
+  numbers.map(item => item * 2);
+  console.log(numbers);
+
+  //summary
+
+  // When building an array containing only numbers
+  const pares = [2,4,6,8,10,12,14,16]
+  // TypeScript infers the TypeScript Type as number[].
+  const paresEx: number[] = [2,4,6,8,10,12,14,16]
+  // When building an array with multiple data types, such as:
+  const arraMult =  [1, true, "str"]
+  // TypeScript infers it in the following way:
+  const arraMultInfer: (number | boolean | string)[] = [1, true, "str"]
+
+})();
